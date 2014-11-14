2a6a350e0d5d31072292e3b7cd7cf39f0198630f
Jonathan Pujol <pujol.jonathan@etu.unice.fr>
2014-11-14 09:56:21 +0100

    Tu vas bien présenter oui ??!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/ServeurTCP.java
+++ src/ServeurTCP.java
@@ -3,12 +3,12 @@ import java.net.*;
 import java.util.Date;
 
 /**
- * La classe ServeurTCP, qui implémente le code du serveur TCP. 
- * La sérialisation se fait en chaîne
- * Un main qui execute le serveur, dont le main contient juste l'attente d'une connexion, 
- * et quand il reçoit des messages, il répond. 
+ * La classe ServeurTCP, qui implémente le code du serveur TCP. La sérialisation
+ * se fait en chaîne Un main qui execute le serveur, dont le main contient juste
+ * l'attente d'une connexion, et quand il reçoit des messages, il répond.
+ * 
  * @author garance & Jo
- *
+ * 
  */
 public class ServeurTCP {
 
@@ -24,25 +24,28 @@ public class ServeurTCP {
             maSocket = new ServerSocket(4444);
             System.out.println("Le serveur est allume : " + new Date());
             while (true) {
-                //On attend une demande de connexion d'un client
+                // On attend une demande de connexion d'un client
                 Socket clientsocket = maSocket.accept();
                 System.out.println("J'ai reçu une connexion !");
                 try {
-                    PrintStream sortie = new PrintStream(clientsocket.getOutputStream());
-                    BufferedReader entree = new BufferedReader(new InputStreamReader(clientsocket.getInputStream()));
+                    PrintStream sortie = new PrintStream(
+                            clientsocket.getOutputStream());
+                    BufferedReader entree = new BufferedReader(
+                            new InputStreamReader(clientsocket.getInputStream()));
                     while (true) {
                         String in = entree.readLine();
-                        if (in == null) break;
-                        //Pour le moment, on répète seulement ce qu'on a lu, donc :
+                        if (in == null)
+                            break;
+                        // Pour le moment, on répète seulement ce qu'on a lu,
+                        // donc :
                         sortie.println(in);
                     }
-                } catch(IOException exception) {
+                } catch (IOException exception) {
                     System.out.println("Erreur: " + exception);
                 }
             }
-        } catch(IOException exception) {
+        } catch (IOException exception) {
             System.out.println("Erreur: " + exception);
         }
     }
-
 }
