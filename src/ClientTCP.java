2e7a0cf6a29c917856aeb4356795e2cba7fba926
Flore <flore.carnage@gmail.com>
2014-11-14 08:59:12 +0100

    + protocole d'app

--- src/ClientTCP.java
+++ src/ClientTCP.java
@@ -19,7 +19,7 @@ public class ClientTCP {
     public ClientTCP() {
         try {
             //Connexion à notre serveur, en local
-            maSocket = new Socket("localhost", 4444);
+            maSocket = new Socket("10.212.123.163", 4444);
             System.out.println("Je suis connecté en local au serveur, maintenant je vais ouvrir les output et input");
             //Ouverture d'un output pour le client sur la socket de communication
             out = new PrintWriter(maSocket.getOutputStream(), true);
