62f0792dd3fe97d291c572f69a5d5185a92be74e
Flore <flore.carnage@gmail.com>
2014-11-18 11:12:19 +0100

    la base de données !!!!

---
+++ src/Donnees.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Garance on 18/11/2014.
+ */
+public class Donnees {
+    List<Personne> donnees;
+
+    public Donnees () {
+        donnees = new ArrayList();
+    }
+
+    public void ajouterPersonne(Personne qn) {
+        donnees.add(qn);
+    }
+
+    public Personne getPersonne(String nom, String apogee) throws PersonneInvalideException {
+        for (Personne p : donnees) {
+            if (p.getNom() == nom && p.getApogee() == apogee) {
+                return p;
+            }
+        }
+        throw new PersonneInvalideException("La personne demandée n'existe pas !");
+    }
+}
